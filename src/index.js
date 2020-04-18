c264592b6278c24b039d4d834a6fe3edcb93fa7f
Leonardo Yudi <lol_leonardohiguti@outlook.com>
2020-04-18 16:18:07 -0300

    adiciona tudo :)

---
+++ src/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+mongoose.connect(process.env.URL_MONGO, { useNewUrlParser: true });
+
+app.use(cors());  //libera que todos os dominios acessem a api
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
+
+app.use(routes);
+
+
+app.listen(3000);
